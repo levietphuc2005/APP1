d2c1f9a20b37965fdcac6d97fc7eb25466370abd
levietphuc2005 <110165117+levietphuc2005@users.noreply.github.com>
2025-02-16 21:15:00 +0700

    16/02/2525

--- photo_memory/lib/Login_page.dart
+++ photo_memory/lib/Login_page.dart
@@ -1,16 +1,52 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, avoid_web_libraries_in_flutter, unused_import, unused_local_variable, library_private_types_in_public_api
+import 'dart:js_util';
+
 import 'package:flutter/material.dart';
 import 'package:photo_memory/main.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _serverController = TextEditingController();
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    _serverController.dispose();
+    super.dispose();
+  }
+
+  void _signIn() {
+    String username = _usernameController.text;
+    String password_ = _passwordController.text;
+    String server = _serverController.text;
+
+    // Hiển thị dữ liệu nhập vào (có thể thay bằng xử lý đăng nhập)
+    if ((username == "Phuc") & (password_ == "123") ){
+      Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const HomePage()),
+    );
+    } else {
+      ScaffoldMessenger.of(context,).showSnackBar(const SnackBar(content: Text('Sai tên đăng nhập hoặc mật khẩu')));
+    // Hiển thị thông báo lỗi
+    }
+
+    // Chuyển đến trang HomePage nếu cần
+    
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar: AppBar(
-        //title: const Text('Login'),
-      //),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -20,51 +56,44 @@ class LoginPage extends StatelessWidget {
               style: TextStyle(fontSize: 20),
             ),
             const SizedBox(height: 20),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
               child: TextField(
-                decoration: InputDecoration(
+                controller: _usernameController,
+                decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Username',
                 ),
               ),
             ),
             const SizedBox(height: 20),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
               child: TextField(
+                controller: _passwordController,
                 obscureText: true,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Password',
                 ),
               ),
             ),
             const SizedBox(height: 20),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
               child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
+                controller: _serverController,
+                decoration: const InputDecoration(
                   border: OutlineInputBorder(),
-                  labelText: 'Sever name',
+                  labelText: 'Server name',
                 ),
               ),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
+              onPressed: _signIn,
               child: const Text('Sign In'),
-              onPressed: () {
-                // Quay lại trang trước đó
-                 Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const HomePage(),
-              ),
-            );
-              },
             ),
-            
           ],
         ),
       ),
--- photo_memory/lib/main.dart
+++ photo_memory/lib/main.dart
@@ -1,12 +1,65 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:photo_memory/Login_page.dart';
 
-void main() => runApp(
-      const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: LoginPage(),
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  final List<Widget> _pages = [
+    const HomePage(),
+    const HomePage(),
+    const HomePage()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Server Status Checker'),
+          backgroundColor: Colors.green,
+          actions:<Widget> [
+            IconButton(         
+            icon: const Icon(Icons.settings),
+            onPressed: () {  }, 
+            ),
+            
+          ],
+
+          ),
+        body: _pages[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: 'List'),
+            BottomNavigationBarItem(icon: Icon(Icons.timeline), label: 'update'),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
       ),
     );
+  }
+}
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -18,30 +71,8 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        leading: IconButton(
-          icon: const Icon(
-            Icons.menu,
-            color: Colors.black87,
-          ),
-          onPressed: () {},
-        ),
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Go to Login'),
-          onPressed: () {
-            // Điều hướng sang trang Login
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const LoginPage(),
-              ),
-            );
-          },
-        ),
-      ),
-     
+      
     );
-    
   }
 }
